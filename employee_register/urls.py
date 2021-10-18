3762246d5e36515d34c3a03fa633d83fafe28144
sivapriya8113 <68690248+sivapriya8113@users.noreply.github.com>
2021-10-18 22:59:23 +0530

    Create urls.py

---
+++ employee_register/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+#from employee_register import views
+from . import views
+from django.contrib import admin
+#from views import register_request, login_request,logout_request
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('add_employee/', views.employee_form,name='employee_insert'),
+    path('<int:id>/', views.employee_form,name='employee_update'),
+    path('delete/<int:id>/',views.employee_delete,name='employee_delete'),
+    path('',views.employee_list,name='employee_list'),
+
+]
