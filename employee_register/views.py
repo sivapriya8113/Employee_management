5fc4ee630c0f720c8ba040e2b8d2392dd8a65e73
sivapriya8113 <68690248+sivapriya8113@users.noreply.github.com>
2021-10-28 23:58:02 +0530

    Update views.py

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
-from .forms import EmployeeForm
+from .forms import EmployeeForm,NumWordForm
 from .models import Employee
 from django.shortcuts import  render, redirect
+from num2words import num2words
+from gtts import gTTS
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
-
+import gtts
+from playsound import playsound
+import os
+import pyttsx3
 
 # Create your views here.
 
@@ -22,6 +27,7 @@ def employee_form(request, id=0):
         else:
             employee = Employee.objects.get(pk=id)
             form = EmployeeForm(instance=employee)
+
         return render(request, "employee_register/employee_form.html", {'form': form})
     else:
         if id == 0:
@@ -30,7 +36,9 @@ def employee_form(request, id=0):
             employee = Employee.objects.get(pk=id)
             form = EmployeeForm(request.POST,instance= employee)
         if form.is_valid():
+            print("@@@@@@@@@@@@@@",form)
             form.save()
+            print("1234567876542345678",form)
         return redirect('/Admin/')
 
 
@@ -39,4 +47,21 @@ def employee_delete(request,id):
     employee.delete()
     print("asdfgh")
     return redirect('/Admin/')
+
+def num_to_word(request):
+    forms = NumWordForm(request.POST or None)
+    print(request.POST)
+    word = 'Please enter your number'
+    if forms.is_valid():
+        number = forms.cleaned_data.get('Number')
+        print(number)
+        word = num2words(number, to='cardinal')
+        print(word)
+    else:
+        print("else")
+    tts = gtts.gTTS(word)
+    tts.save("Speech.mp3")
+    #playsound("Speech.mp3",False)
+    context = {"form": forms, "word": word}
+    return render(request, 'employee_register/num_to_word.html', context=context)
 
